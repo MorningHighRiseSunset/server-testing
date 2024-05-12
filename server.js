2691f1c3d1001192a61dcdcf098ac74602bb4768
DestroyLonelyIsReal <147635787+DestroyLonelyIsReal@users.noreply.github.com>
2024-05-12 10:19:52 -0700

    Add Socket.IO and Express code for multiplayer game

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('setName', (name) => {
+    console.log(`User set name: ${name}`);
+    socket.name = name;
+    io.emit('setName', name);
+  });
+
+  socket.on('sendMessage', (message) => {
+    console.log(`Received message: ${message}`);
+    io.emit('receiveMessage', `${socket.name}: ${message}`);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
